4ed3bb0e1a93cff4cb98f9d12c9f4d30d2e792ac
Brian Broll <brian.broll@gmail.com>
2016-03-14 21:04:50 -0500

    Fixes #18 Added simple UI (w/ dev mode)
    WIP #18 set the layout to npm release
    
    Added breadcrumbheader, autoviz. WIP #18

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -9,14 +9,34 @@ var config = require('webgme/config/config.default'),
 
 // The paths can be loaded from the webgme-setup.json
 config.plugin.basePaths.push('src/plugins');
+config.visualization.layout.basePaths.push('node_modules/webgme-chflayout/src/layouts');
+
+
+
+config.visualization.panelPaths.push('node_modules/webgme-fab/src/visualizers/panels');
+config.visualization.panelPaths.push('node_modules/webgme-breadcrumbheader/src/visualizers/panels');
+config.visualization.panelPaths.push('node_modules/webgme-autoviz/src/visualizers/panels');
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push('./src/visualizers/Visualizers.json');
 // Add requirejs paths
+config.requirejsPaths = {
+  'AutoViz': 'panels/AutoViz/AutoVizPanel',
+  'BreadcrumbHeader': 'panels/BreadcrumbHeader/BreadcrumbHeaderPanel',
+  'FloatingActionButton': 'panels/FloatingActionButton/FloatingActionButtonPanel',
+  'CHFLayout': 'node_modules/webgme-chflayout/src/layouts/CHFLayout',
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
+  'panels/AutoViz': './node_modules/webgme-autoviz/src/visualizers/panels/AutoViz',
+  'widgets/AutoViz': './node_modules/webgme-autoviz/src/visualizers/widgets/AutoViz',
+  'panels/BreadcrumbHeader': './node_modules/webgme-breadcrumbheader/src/visualizers/panels/BreadcrumbHeader',
+  'widgets/BreadcrumbHeader': './node_modules/webgme-breadcrumbheader/',
+  'panels/FloatingActionButton': './node_modules/webgme-fab/src/visualizers/panels/FloatingActionButton',
+  'widgets/FloatingActionButton': './node_modules/webgme-fab/src/visualizers/widgets/FloatingActionButton'
+};
 
-
-
+config.visualization.layout.default = 'CHFLayout';
 config.mongo.uri = 'mongodb://127.0.0.1:27017/deepforge';
 validateConfig(config);
 module.exports = config;
