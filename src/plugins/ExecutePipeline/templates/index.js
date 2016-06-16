b65c0b740a288c337bb5c08c43d4984e009a22ff
Brian Broll <brian.broll@vanderbilt.edu>
2016-06-16 15:07:18 -0500

    Revert "Added regex to remove console colors. Fixes #306 " (#311)

--- src/plugins/ExecutePipeline/templates/index.js
+++ src/plugins/ExecutePipeline/templates/index.js
@@ -11,7 +11,7 @@ define([
     DESERIALIZE
 ) {
 
-    var BASH = `th init.lua  2>&1 | sed -e 's/[[:cntrl:]]//g' -e 's/\\[32;1m//g' -e 's/\\[0;36m//g' -e 's/\\[33;1m//g' -e 's/\\[35;1m//g' -e 's/\\[37;1m//g' -e 's/\\[0m//g'`;
+    var BASH = 'th init.lua  2>&1';
     return {
         BASH,
         ENTRY,
