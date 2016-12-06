994a8b6f39951f39b64fb801f12c89c00c8e0b9a
Brian Broll <brian.broll@vanderbilt.edu>
2016-12-05 18:32:23 -0600

    Removed custom disable checking. Fixes #917 (#918)
    * Removed custom disable checking. Fixes #917
    
    * WIP #917 Removed unused variable
    
    * WIP #917 removed unused var

--- src/visualizers/widgets/ArchEditor/SelectionManager.js
+++ src/visualizers/widgets/ArchEditor/SelectionManager.js
@@ -1,4 +1,4 @@
-/* globals _, WebGMEGlobal, define*/
+/* globals _, define*/
 define([
     'widgets/EasyDAG/SelectionManager',
     'deepforge/viz/Buttons'
@@ -7,7 +7,6 @@ define([
     Buttons
 ) {
 
-    var client = WebGMEGlobal.Client;
     var SelectionManager = function() {
         ManagerBase.apply(this, arguments);
     };
@@ -15,8 +14,7 @@ define([
     _.extend(SelectionManager.prototype, ManagerBase.prototype);
 
     SelectionManager.prototype.createActionButtons = function(width, height) {
-        var disabled,
-            btn;
+        var btn;
 
         ManagerBase.prototype.createActionButtons.call(this, width, height);
 
@@ -29,32 +27,18 @@ define([
                 y: height/2
             });
         } else {
-            disabled = !this._isCustomLayer();
             // Check that the base type
             btn = new Buttons.GoToBase({
                 $pEl: this.$selection,
                 context: this._widget,
                 title: 'Edit layer definition',
                 item: this.selectedItem,
-                disabled: disabled,
                 x: width,
                 y: 0
             });
         }
 
         return btn;
-    };
-
-    SelectionManager.prototype._isCustomLayer = function() {
-        var node = client.getNode(this.selectedItem.id),
-            attrNames;
-
-        if (node) {
-            attrNames = node.getAttributeNames();
-            return attrNames.indexOf('code') !== -1;
-        }
-
-        return false;
     };
 
     return SelectionManager;
