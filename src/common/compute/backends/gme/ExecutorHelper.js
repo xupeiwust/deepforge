3bdabb3bdf0bb8988e2043f437033e004ecd1b45
Brian Broll <brian.broll@gmail.com>
2019-09-19 15:24:04 -0500

    Fixed url resolution for gme compute backend. Fixes #1238 (#1239)

--- src/common/compute/backends/gme/ExecutorHelper.js
+++ src/common/compute/backends/gme/ExecutorHelper.js
@@ -8,14 +8,15 @@ define([
     module,
     Q
 ) {
+
     const WORKER_ENDPOINT = '/rest/executor/worker';
     const JOBS_ENDPOINT = '/rest/executor';
-    const values = dict => Object.keys(dict).map(k => dict[k]);
     const ExecutorHelper = {};
 
     ExecutorHelper.url = function(urlPath) {
         if (typeof window === 'undefined') {
-            const configPath = module.uri.replace('src/common/ExecutorHelper.js', 'config/index.js');
+            const modulePath = 'src/common/compute/backends/gme/ExecutorHelper.js';
+            const configPath = module.uri.replace(modulePath, 'config/index.js');
             const gmeConfig = require.nodeRequire(configPath);
             return `http://127.0.0.1:${gmeConfig.server.port}${urlPath}`;
         }
@@ -39,7 +40,7 @@ define([
 
     ExecutorHelper.getWorkers = function() {
         return this.get(WORKER_ENDPOINT)
-            .then(workerDict => values(workerDict));
+            .then(workerDict => Object.values(workerDict));
     };
 
     ExecutorHelper.getJobs = function() {
