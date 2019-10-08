aab6f65cd9e833f12e4c458d0d3bf953895c9c2e
Brian Broll <brian.broll@gmail.com>
2019-10-08 12:28:10 -0500

    Wrap Compute in IIFE to avoid global var. Fixes #1246 (#1247)

--- src/common/compute/index.js
+++ src/common/compute/index.js
@@ -1,55 +1,55 @@
 /*globals define, requirejs */
-const COMPUTE_BACKENDS = ['gme', 'local'];
-define([
-    'q',
-    'module',
-    'deepforge/compute/backends/ComputeBackend',
-].concat(COMPUTE_BACKENDS.map(name => `text!deepforge/compute/backends/${name}/metadata.json`)),
-function(
-    Q,
-    module,
-    ComputeBackend,
-) {
-    const Compute = {};
+(function() {
+    const COMPUTE_BACKENDS = ['gme', 'local'];
+    define([
+        'module',
+        'deepforge/compute/backends/ComputeBackend',
+    ].concat(COMPUTE_BACKENDS.map(name => `text!deepforge/compute/backends/${name}/metadata.json`)),
+    function(
+        module,
+        ComputeBackend,
+    ) {
+        const Compute = {};
 
-    Compute.getComponentId = function() {
-        return 'Compute';
-    };
+        Compute.getComponentId = function() {
+            return 'Compute';
+        };
 
-    Compute.getAvailableBackends = function() {
-        const settings = {backends: COMPUTE_BACKENDS};  // all by default
-        if (require.isBrowser) {
-            const ComponentSettings = requirejs('js/Utils/ComponentSettings');
-            ComponentSettings.resolveWithWebGMEGlobal(
-                settings,
-                this.getComponentId()
-            );
-        } else {  // Running in NodeJS
-            const path = require('path');
-            const dirname = path.dirname(module.uri);
-            const deploymentSettings = JSON.parse(requirejs('text!' + dirname + '/../../../config/components.json'));
-            Object.assign(settings, deploymentSettings[this.getComponentId()]);
-        }
+        Compute.getAvailableBackends = function() {
+            const settings = {backends: COMPUTE_BACKENDS};  // all by default
+            if (require.isBrowser) {
+                const ComponentSettings = requirejs('js/Utils/ComponentSettings');
+                ComponentSettings.resolveWithWebGMEGlobal(
+                    settings,
+                    this.getComponentId()
+                );
+            } else {  // Running in NodeJS
+                const path = require('path');
+                const dirname = path.dirname(module.uri);
+                const deploymentSettings = JSON.parse(requirejs('text!' + dirname + '/../../../config/components.json'));
+                Object.assign(settings, deploymentSettings[this.getComponentId()]);
+            }
 
-        return settings.backends;
-    };
+            return settings.backends;
+        };
 
-    Compute.getBackend = function(id) {
-        const metadata = this.getMetadata(id);
-        return new ComputeBackend(id, metadata);
-    };
+        Compute.getBackend = function(id) {
+            const metadata = this.getMetadata(id);
+            return new ComputeBackend(id, metadata);
+        };
 
-    Compute.getMetadata = function(id) {
-        id = id.toLowerCase();
-        if (!COMPUTE_BACKENDS.includes(id)) {
-            throw new Error(`Compute backend not found: ${id}`);
-        }
+        Compute.getMetadata = function(id) {
+            id = id.toLowerCase();
+            if (!COMPUTE_BACKENDS.includes(id)) {
+                throw new Error(`Compute backend not found: ${id}`);
+            }
 
-        const relativePath = `backends/${id}/metadata.json`;
-        const metadata = JSON.parse(requirejs(`text!deepforge/compute/${relativePath}`));
-        metadata.id = id;
-        return metadata;
-    };
+            const relativePath = `backends/${id}/metadata.json`;
+            const metadata = JSON.parse(requirejs(`text!deepforge/compute/${relativePath}`));
+            metadata.id = id;
+            return metadata;
+        };
 
-    return Compute;
-});
+        return Compute;
+    });
+})();
