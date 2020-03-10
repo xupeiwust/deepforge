45d77ef456e90497c950e2215a5a4e7f51ab3729
Brian Broll <brian.broll@gmail.com>
2020-03-10 11:53:44 -0500

    Add aws-sdk to sciserver compute backend. Closes #1464 (#1471)

--- src/common/compute/backends/sciserver-compute/files/prepare-and-run.sh
+++ src/common/compute/backends/sciserver-compute/files/prepare-and-run.sh
@@ -2,7 +2,7 @@ RESULTS_DIR=$(pwd)
 JOB_DIR=$1
 
 cd $JOB_DIR;
-npm install requirejs@2.3.5 rimraf@^2.4.0 superagent@3.8.3 @babel/runtime@^7.7.2 q@1.5.1 node-fetch@2.6.0 agentkeepalive@3.4.1 > /dev/null
+npm install requirejs@2.3.5 rimraf@^2.4.0 superagent@3.8.3 @babel/runtime@^7.7.2 q@1.5.1 node-fetch@2.6.0 agentkeepalive@3.4.1 aws-sdk@2.624.0 > /dev/null
 pip install simplejson > /dev/null
 
 node start.js
