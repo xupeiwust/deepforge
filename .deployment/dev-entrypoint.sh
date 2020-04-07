7a90714431d941bee6fd32aa0e8b4d99a6361a64
Brian Broll <brian.broll@gmail.com>
2020-04-07 13:10:49 -0500

    Move dockerfiles to docker/. Closes #1513 (#1598)
    * Moved docker files to docker/
    
    * Update dockerfile locations in github action
    
    * Remove unnecessary conda commands in new entrypoint

--- .deployment/dev-entrypoint.sh
+++ .deployment/dev-entrypoint.sh
@@ -4,4 +4,4 @@ source activate deepforge-server
 pip uninstall tensorflow -y
 conda install tensorflow==1.14 -y
 
-source activate ${DEEPFORGE_CONDA_ENV} && deepforge start --server
+deepforge start --server
