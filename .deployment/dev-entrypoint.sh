7a5b2db571b866a8ebbf66e83abf409abaff6bbd
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-04-08 11:01:01 -0500

    Disable local compute in deployment. Closes #1605 (#1606)
    * Remove local compute from deployment. Closes #1605
    
    * WIP- Install a jq binary as sudo not available in entrypoint
    
    Co-authored-by: Brian Broll <brian.broll@gmail.com>

--- .deployment/dev-entrypoint.sh
+++ .deployment/dev-entrypoint.sh
@@ -4,4 +4,10 @@ source activate deepforge-server
 pip uninstall tensorflow -y
 conda install tensorflow==1.14 -y
 
+# Install jq and remove local from config/components.json file
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O jq && chmod +x jq
+
+< config/components.json  ./jq '.Compute.backends=(.Compute.backends | map(select(. != "local")))' \
+> config/components2.json && mv config/components2.json config/components.json
+
 deepforge start --server
