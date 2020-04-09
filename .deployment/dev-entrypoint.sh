cb9337e949b4dd5583e96ca9d0b1fdee659a93cc
Brian Broll <brian.broll@gmail.com>
2020-04-08 21:25:35 -0500

    Disable GME storage on deployment. Closes #1618 (#1619)

--- .deployment/dev-entrypoint.sh
+++ .deployment/dev-entrypoint.sh
@@ -8,7 +8,9 @@ conda install tensorflow==1.14 -y
 wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O jq && chmod +x jq
 
 < config/components.json  ./jq '.Compute.backends=(.Compute.backends | map(select(. != "local")))' \
-> config/components2.json && mv config/components2.json config/components.json
+> config/components2.json
+< config/components2.json  ./jq '.Storage.backends=(.Storage.backends | map(select(. != "gme")))' \
+> config/components.json
 
 deepforge start --server
 
