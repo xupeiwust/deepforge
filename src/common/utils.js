c0edc3e04aedb8873459b77640fc88158e711032
Brian Broll <brian.broll@gmail.com>
2020-04-20 14:00:29 -0500

    Remove old getSetterSchema from v1 (#1663)

--- src/common/utils.js
+++ src/common/utils.js
@@ -8,37 +8,6 @@
         module.exports = (root.utils = factory());
     }
 }(this, function() {
-    var isBoolean = txt => {
-        return typeof txt === 'boolean' || (txt === 'false' || txt === 'true');
-    };
-
-    var getSetterSchema = function(name, setters, defaults) {
-        var values,
-            schema = setters[name];
-
-        if (defaults.hasOwnProperty(name)) {
-            schema.default = defaults[name];
-        }
-        schema.type = 'string';
-        if (schema.setterType === 'const') {
-            values = Object.keys(schema.setterFn);
-            schema.isEnum = true;
-            schema.enumValues = values;
-            if (values.every(isBoolean)) {
-                if (!defaults.hasOwnProperty(name) && values.length === 1) {
-                    // there is only a method to toggle the flag to true/false, 
-                    // then the default must be the other one
-                    schema.default = values[0] === 'true' ? false : true;
-                }
-
-                if (isBoolean(schema.default)) {
-                    schema.type = 'boolean';
-                }
-            }
-        }
-        return schema;
-    };
-
     var abbrWord = function(word) {  // camelcase
         word = word.substring(0, 1).toUpperCase() + word.substring(1);
         return word.split(/[a-z]+/g).join('').toLowerCase();
@@ -59,7 +28,6 @@
 
     // Resolving stdout
     var resolveCarriageReturns = function(text) {
-        // resolve \r
         var lines,
             chars,
             result,
@@ -111,7 +79,6 @@
     };
 
     return {
-        getSetterSchema,
         resolveCarriageReturns,
         abbr,
         withTimeout,
