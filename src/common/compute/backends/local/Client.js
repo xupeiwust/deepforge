bef1cf57fdf98fbea2c4e24962d61c7fb80507a3
Brian Broll <brian.broll@gmail.com>
2020-05-05 10:01:56 -0500

    WIP refactor compute backend, client, local compute

--- src/common/compute/backends/local/Client.js
+++ src/common/compute/backends/local/Client.js
@@ -46,195 +46,195 @@ define([
     const symlink = promisify(fs.symlink);
     const touch = async name => await closeFile(await openFile(name, 'w'));
 
-    const LocalExecutor = function() {
-        ComputeClient.apply(this, arguments);
+    class LocalExecutor extends ComputeClient {
+        constructor () {
+            super(...arguments);
 
-        this.jobQueue = [];
-        this.currentJob = null;
-        this.subprocess = null;
-        this.canceled = false;
-    };
-
-    LocalExecutor.prototype = Object.create(ComputeClient.prototype);
-
-    LocalExecutor.prototype.cancelJob = function(jobInfo) {
-        const {hash} = jobInfo;
-
-        if (this.currentJob === hash) {
-            this.canceled = true;
-            this.subprocess.kill();
-        } else if (this.jobQueue.includes(hash)) {
-            const i = this.jobQueue.indexOf(hash);
-            this.jobQueue.splice(i, 1);
-            this._onJobCompleted(hash, new JobResults(this.CANCELED));
-        }
-    };
-
-    LocalExecutor.prototype.getStatus = async function(jobInfo) {
-        const {hash} = jobInfo;
-        if (hash === this.currentJob) {
-            return this.RUNNING;
-        } else if (this.jobQueue.includes(hash)) {
-            return this.QUEUED;
-        } else {
-            return await this._getJobFile(hash, 'status.txt', 'Job Not Found');
-        }
-    };
-
-    LocalExecutor.prototype.getConsoleOutput = async function(job) {
-        const msg = 'Console output data not found.';
-        return await this._getJobFile(job.hash, STDOUT_FILE, msg);
-    };
-
-    LocalExecutor.prototype.getResultsInfo = async function(job) {
-        const msg = 'Metadata about result types not found.';
-        const resultsTxt = await this._getJobFile(job.hash, 'results.json', msg);
-        return JSON.parse(resultsTxt);
-    };
-
-    LocalExecutor.prototype._getJobFile = async function(hash, name, notFoundMsg) {
-        const filename = path.join(this._getWorkingDir(hash), name);
-        try {
-            return await readFile(filename, 'utf8');
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                throw new Error(notFoundMsg);
-            }
-            throw err;
-        }
-    };
-
-    LocalExecutor.prototype.createJob = async function(hash) {
-        this.jobQueue.push(hash);
-        this._processNextJob();
-
-        return {hash};
-    };
-
-    LocalExecutor.prototype._onJobCompleted = async function(hash, jobResults) {
-        if (hash === this.currentJob) {
+            this.jobQueue = [];
             this.currentJob = null;
+            this.subprocess = null;
+            this.canceled = false;
         }
 
-        const tmpdir = this._getWorkingDir(hash);
-        //await this._cleanDirectory(tmpdir);
-        await writeFile(path.join(tmpdir, 'status.txt'), jobResults.status);
+        cancelJob (jobInfo) {
+            const {hash} = jobInfo;
 
-        this.emit('update', hash, jobResults.status);
-        this.emit('end', hash, jobResults);
-        this._processNextJob();
-    };
-
-    LocalExecutor.prototype._cleanDirectory = async function(workdir) {
-        const SKIP_FILES = ['results.json', STDOUT_FILE];
-        const files = (await readdir(workdir))
-            .filter(name => !SKIP_FILES.includes(name))
-            .map(name => path.join(workdir, name));
-
-        return Promise.all(files.map(file => rm_rf(file)));
-    };
-
-    LocalExecutor.prototype._processNextJob = function() {
-        if (this.currentJob) return;
-
-        this.currentJob = this.jobQueue.shift();
-        if (this.currentJob) {
-            return this._createJob(this.currentJob);
+            if (this.currentJob === hash) {
+                this.canceled = true;
+                this.subprocess.kill();
+            } else if (this.jobQueue.includes(hash)) {
+                const i = this.jobQueue.indexOf(hash);
+                this.jobQueue.splice(i, 1);
+                this._onJobCompleted(hash, new JobResults(this.CANCELED));
+            }
         }
-    };
 
-    LocalExecutor.prototype._getWorkingDir = function(hash) {
-        return path.join(os.tmpdir(), `deepforge-local-exec-${hash}`);
-    };
-
-    LocalExecutor.prototype._createJob = async function(hash) {
-        const jobInfo = {hash};
-        this.emit('update', jobInfo.hash, this.PENDING);
-        const tmpdir = this._getWorkingDir(hash);
-        try {
-            await mkdir(tmpdir);
-        } catch (err) {
-            if (err.code === 'EEXIST') {
-                await rm_rf(tmpdir);
-                await mkdir(tmpdir);
+        async getStatus (jobInfo) {
+            const {hash} = jobInfo;
+            if (hash === this.currentJob) {
+                return this.RUNNING;
+            } else if (this.jobQueue.includes(hash)) {
+                return this.QUEUED;
             } else {
+                return await this._getJobFile(hash, 'status.txt', 'Job Not Found');
+            }
+        }
+
+        async getConsoleOutput (job) {
+            const msg = 'Console output data not found.';
+            return await this._getJobFile(job.hash, STDOUT_FILE, msg);
+        }
+
+        async getResultsInfo (job) {
+            const msg = 'Metadata about result types not found.';
+            const resultsTxt = await this._getJobFile(job.hash, 'results.json', msg);
+            return JSON.parse(resultsTxt);
+        }
+
+        async _getJobFile (hash, name, notFoundMsg) {
+            const filename = path.join(this._getWorkingDir(hash), name);
+            try {
+                return await readFile(filename, 'utf8');
+            } catch (err) {
+                if (err.code === 'ENOENT') {
+                    throw new Error(notFoundMsg);
+                }
                 throw err;
             }
         }
-        this.logger.info('created working directory at', tmpdir);
 
-        // Fetch the required files from deepforge
-        await this.prepareWorkspace(hash, tmpdir);
+        async createJob (hash) {
+            this.jobQueue.push(hash);
+            this._processNextJob();
 
-        // Spin up a subprocess
-        const config = JSON.parse(await readFile(tmpdir.replace(path.sep, '/') + '/executor_config.json', 'utf8'));
+            return {hash};
+        }
 
-        const env = process.env;
-        env.DEEPFORGE_ROOT = DEEPFORGE_ROOT;
-        const options = {
-            cwd: tmpdir,
-            env,
-        };
-        this.logger.info(`Running ${config.cmd} ${config.args.join(' ')}`);
-        this.subprocess = spawn(config.cmd, config.args, options);
-        this.emit('update', jobInfo.hash, this.RUNNING);
-        this.subprocess.stdout.on('data', data => this.onConsoleOutput(tmpdir, hash, data));
+        async _onJobCompleted (hash, jobResults) {
+            if (hash === this.currentJob) {
+                this.currentJob = null;
+            }
 
-        this.subprocess.on('close', async code => {
-            const status = this.canceled ? this.CANCELED :
-                (code !== 0 ? this.FAILED : this.SUCCESS);
+            const tmpdir = this._getWorkingDir(hash);
+            //await this._cleanDirectory(tmpdir);
+            await writeFile(path.join(tmpdir, 'status.txt'), jobResults.status);
 
-            const jobResults = new JobResults(status);
-            this.canceled = false;
+            this.emit('update', hash, jobResults.status);
+            this.emit('end', hash, jobResults);
+            this._processNextJob();
+        }
 
-            this._onJobCompleted(hash, jobResults);
-        });
-    };
+        async _cleanDirectory (workdir) {
+            const SKIP_FILES = ['results.json', STDOUT_FILE];
+            const files = (await readdir(workdir))
+                .filter(name => !SKIP_FILES.includes(name))
+                .map(name => path.join(workdir, name));
 
-    LocalExecutor.prototype.onConsoleOutput = async function(workdir, hash, data) {
-        const filename = path.join(workdir, STDOUT_FILE);
-        appendFile(filename, data);
-        this.emit('data', hash, data);
-    };
+            return Promise.all(files.map(file => rm_rf(file)));
+        }
 
-    LocalExecutor.prototype._getAllFiles = async function(workdir) {
-        const dirs = (await readdir(workdir))
-            .filter(n => !n.includes('node_modules'))
-            .map(name => path.join(workdir, name));
-        const files = [];
+        _processNextJob () {
+            if (this.currentJob) return;
 
-        // Read each directory
-        while (dirs.length) {
-            const abspath = dirs.shift();
-            const isDirectory = (await statFile(abspath)).isDirectory();
-            if (isDirectory) {
-                const childpaths = (await readdir(abspath))
-                    .map(name => path.join(abspath, name));
-                dirs.push.apply(dirs, childpaths);
-            } else {
-                files.push(abspath);
+            this.currentJob = this.jobQueue.shift();
+            if (this.currentJob) {
+                return this._createJob(this.currentJob);
             }
         }
 
-        return files;
-    };
+        _getWorkingDir (hash) {
+            return path.join(os.tmpdir(), `deepforge-local-exec-${hash}`);
+        }
 
-    LocalExecutor.prototype.prepareWorkspace = async function(hash, dirname) {
-        this.logger.info('about to fetch job data');
-        const content = new Buffer(new Uint8Array(await this.blobClient.getObject(hash)));  // TODO: Handle errors...
-        const zipPath = path.join(dirname, `${hash}.zip`);
-        await writeFile(zipPath, content);
-        this.logger.info(`Fetched job data: ${zipPath}`);
+        async _createJob (hash) {
+            const jobInfo = {hash};
+            this.emit('update', jobInfo.hash, this.PENDING);
+            const tmpdir = this._getWorkingDir(hash);
+            try {
+                await mkdir(tmpdir);
+            } catch (err) {
+                if (err.code === 'EEXIST') {
+                    await rm_rf(tmpdir);
+                    await mkdir(tmpdir);
+                } else {
+                    throw err;
+                }
+            }
+            this.logger.info('created working directory at', tmpdir);
 
-        this.logger.info(`unzipping ${zipPath} in ${dirname}`);
-        await unzip(zipPath, dirname);
+            // Fetch the required files from deepforge
+            await this.prepareWorkspace(hash, tmpdir);
 
-        // Set up a symbolic link to the node_modules
-        await symlink(NODE_MODULES, path.join(dirname, 'node_modules'));
+            // Spin up a subprocess
+            const config = JSON.parse(await readFile(tmpdir.replace(path.sep, '/') + '/executor_config.json', 'utf8'));
 
-        // Prepare for the stdout
-        await touch(path.join(dirname, STDOUT_FILE));
-    };
+            const env = process.env;
+            env.DEEPFORGE_ROOT = DEEPFORGE_ROOT;
+            const options = {
+                cwd: tmpdir,
+                env,
+            };
+            this.logger.info(`Running ${config.cmd} ${config.args.join(' ')}`);
+            this.subprocess = spawn(config.cmd, config.args, options);
+            this.emit('update', jobInfo.hash, this.RUNNING);
+            this.subprocess.stdout.on('data', data => this.onConsoleOutput(tmpdir, hash, data));
+
+            this.subprocess.on('close', async code => {
+                const status = this.canceled ? this.CANCELED :
+                    (code !== 0 ? this.FAILED : this.SUCCESS);
+
+                const jobResults = new JobResults(status);
+                this.canceled = false;
+
+                this._onJobCompleted(hash, jobResults);
+            });
+        }
+
+        async onConsoleOutput (workdir, hash, data) {
+            const filename = path.join(workdir, STDOUT_FILE);
+            appendFile(filename, data);
+            this.emit('data', hash, data);
+        }
+
+        async _getAllFiles (workdir) {
+            const dirs = (await readdir(workdir))
+                .filter(n => !n.includes('node_modules'))
+                .map(name => path.join(workdir, name));
+            const files = [];
+
+            // Read each directory
+            while (dirs.length) {
+                const abspath = dirs.shift();
+                const isDirectory = (await statFile(abspath)).isDirectory();
+                if (isDirectory) {
+                    const childpaths = (await readdir(abspath))
+                        .map(name => path.join(abspath, name));
+                    dirs.push.apply(dirs, childpaths);
+                } else {
+                    files.push(abspath);
+                }
+            }
+
+            return files;
+        }
+
+        async prepareWorkspace (hash, dirname) {
+            this.logger.info('about to fetch job data');
+            const content = new Buffer(new Uint8Array(await this.blobClient.getObject(hash)));  // TODO: Handle errors...
+            const zipPath = path.join(dirname, `${hash}.zip`);
+            await writeFile(zipPath, content);
+            this.logger.info(`Fetched job data: ${zipPath}`);
+
+            this.logger.info(`unzipping ${zipPath} in ${dirname}`);
+            await unzip(zipPath, dirname);
+
+            // Set up a symbolic link to the node_modules
+            await symlink(NODE_MODULES, path.join(dirname, 'node_modules'));
+
+            // Prepare for the stdout
+            await touch(path.join(dirname, STDOUT_FILE));
+        }
+    }
 
     async function unzip(filename, dirname) {
         const args = UNZIP_ARGS.concat(path.basename(filename));
