c9168bfd75e672cf4fa6bb36e933b8b0b1cf7179
Brian Broll <brian.broll@gmail.com>
2020-05-05 07:20:20 -0500

    Combine COMPLETE/CLOSE messages

--- src/common/compute/interactive/task.js
+++ src/common/compute/interactive/task.js
@@ -10,24 +10,26 @@ define([
 ) {
 
     class Task extends EventEmitter {
-        constructor(ws, cmd) {
+        constructor(ws, msg) {
             super();
             this.ws = ws;
-            this.cmd = cmd;
+            this.msg = msg;
         }
 
         async run() {
             const deferred = utils.defer();
 
-            this.ws.send(this.cmd);
+            this.ws.send(this.msg.encode());
             this.ws.onmessage = async wsMsg => {
                 const data = wsMsg.data instanceof Blob ?
                     await wsMsg.data.text() : wsMsg.data;
 
                 const msg = Message.decode(data);
+                console.log('received msg:', msg);
                 this.emitMessage(msg);
-                if (msg.type === Message.CLOSE) {
+                if (msg.type === Message.COMPLETE) {
                     this.ws.onmessage = null;
+                    console.log('resolving', this.msg);
                     deferred.resolve();
                 }
             };
--- src/routers/InteractiveCompute/job-files/start-session.js
+++ src/routers/InteractiveCompute/job-files/start-session.js
@@ -2,18 +2,43 @@ const Message = require('./message');
 const {spawn} = require('child_process');
 const [, , SERVER_URL, ID] = process.argv;
 const WebSocket = require('ws');
+const fs = require('fs').promises;
+const path = require('path');
 
 const ws = new WebSocket(SERVER_URL);
 ws.on('open', () => ws.send(ID));
 
 // TODO: Should this run in a single subprocess or in many?
 // For now, let's run it in a single subprocess...
-ws.on('message', function(data) {
+ws.on('message', async function(data) {
     // TODO: Run the command and send the results back
     // TODO: Queue the commands here?
-    const [cmd, ...opts] = data.split(' ');
-    const subprocess = spawn(cmd, opts);
-    subprocess.on('close', code => ws.send(Message.encode(Message.CLOSE, code)));
-    subprocess.stdout.on('data', data => ws.send(Message.encode(Message.STDOUT, data)));
-    subprocess.stderr.on('data', data => ws.send(Message.encode(Message.STDERR, data)));
+    const msg = Message.decode(data);
+    if (msg.type === Message.RUN) {
+        const [cmd, ...opts] = msg.data.split(' ');
+        const subprocess = spawn(cmd, opts);
+        subprocess.on('close', code => ws.send(Message.encode(Message.COMPLETE, code)));
+        subprocess.stdout.on('data', data => ws.send(Message.encode(Message.STDOUT, data)));
+        subprocess.stderr.on('data', data => ws.send(Message.encode(Message.STDERR, data)));
+    } else if (msg.type === Message.ADD_ARTIFACT) {
+        console.log('adding artifact...');
+        const [name, dataInfo, type] = msg.data;
+        console.log(name, dataInfo, type);
+        console.log(msg);
+        await mkdirp(['artifacts', name]);
+        // TODO: make artifacts/ directory if needed?
+    }
 });
+
+async function mkdirp() {
+    const dirs = Array.prototype.slice.call(arguments);
+    for (let i = 0; i < dirs.length; i++) {
+        try {
+            await fs.mkdir(dirs[i]);
+        } catch (err) {
+            if (err.code !== 'EEXIST') {
+                throw err;
+            }
+        }
+    }
+}
