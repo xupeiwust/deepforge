07c3873ece3d2a19cf202c65c5f7d34d84e74ea6
Brian Broll <brian.broll@gmail.com>
2020-05-14 13:01:09 -0500

    Read command, args from executor_config.json. Closes #1700 (#1701)

--- src/common/compute/backends/sciserver-compute/files/prepare-and-run.sh
+++ src/common/compute/backends/sciserver-compute/files/prepare-and-run.sh
@@ -10,5 +10,7 @@ cd $JOB_DIR;
 npm install requirejs@2.3.5 rimraf@^2.4.0 superagent@3.8.3 @babel/runtime@^7.7.2 q@1.5.1 node-fetch@2.6.0 agentkeepalive@3.4.1 aws-sdk@2.624.0 > /dev/null
 pip install simplejson > /dev/null
 
-node start.js
+CMD=$(node -e 'console.log(require("./executor_config.json").cmd)')
+ARGS=$(node -e 'console.log(require("./executor_config.json").args.join(" "))')
+$CMD $ARGS
 cp $JOB_DIR/results.json $RESULTS_DIR/results.json
