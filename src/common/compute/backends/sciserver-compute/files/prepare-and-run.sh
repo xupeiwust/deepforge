744ac4717862961fdda7d7afda95b151f83bc050
Brian Broll <brian.broll@gmail.com>
2020-08-04 13:32:25 -0500

    Install plotly to sciserver compute image. Fixes #1826 (#1828)
    * Install plotly to sciserver compute image. Fixes #1826
    
    * Update src/common/compute/backends/sciserver-compute/files/prepare-and-run.sh
    
    Co-authored-by: Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
    
    Co-authored-by: Umesh Timalsina <umesh.timalsina@vanderbilt.edu>

--- src/common/compute/backends/sciserver-compute/files/prepare-and-run.sh
+++ src/common/compute/backends/sciserver-compute/files/prepare-and-run.sh
@@ -3,6 +3,7 @@ JOB_DIR=$1
 
 source activate deepforge;
 cd $JOB_DIR;
+conda run -n deepforge pip install plotly
 CMD=$(conda run -n deepforge node -e 'console.log(require("./executor_config.json").cmd)')
 ARGS=$(conda run -n deepforge node -e 'console.log(require("./executor_config.json").args.join(" "))')
 conda run -n deepforge $CMD $ARGS
