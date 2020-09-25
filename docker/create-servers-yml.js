4503ae4ae0a0dfb7eb90b1b5283b42e84014e57f
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-09-25 13:59:22 -0500

    WIP- Fix deployment issue with language servers

--- docker/create-servers-yml.js
+++ docker/create-servers-yml.js
@@ -1,7 +1,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
-const langServers = require(path.resolve(__dirname, '..', '.deployment', 'languageServers'));
+const langServers = require(path.resolve(__dirname, '..', 'utils', 'languageServers'));
 
 function dumpLangServerYaml(fileName) {
     const serversYml = {
