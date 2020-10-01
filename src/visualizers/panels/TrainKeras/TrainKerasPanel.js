2df7dbaa0963281d765da2eec51b1475033e9a68
Brian Broll <brian.broll@gmail.com>
2020-10-01 11:09:57 -0500

    Refactor TrainKeras to use session in the controller. Closes #1938

--- src/visualizers/panels/TrainKeras/TrainKerasPanel.js
+++ src/visualizers/panels/TrainKeras/TrainKerasPanel.js
@@ -5,97 +5,66 @@
 
 define([
     'js/PanelBase/PanelBaseWithHeader',
-    'js/PanelManager/IActivePanel',
+    'panels/InteractiveEditor/InteractiveEditorPanel',
     'widgets/TrainKeras/TrainKerasWidget',
-    './TrainKerasControl'
+    './TrainKerasControl',
 ], function (
     PanelBaseWithHeader,
-    IActivePanel,
+    InteractiveEditorPanel,
     TrainKerasWidget,
-    TrainKerasControl
+    TrainKerasControl,
 ) {
     'use strict';
 
-    function TrainKerasPanel(layoutManager, params) {
-        var options = {};
-        //set properties from options
-        options[PanelBaseWithHeader.OPTIONS.LOGGER_INSTANCE_NAME] = 'TrainKerasPanel';
-        options[PanelBaseWithHeader.OPTIONS.FLOATING_TITLE] = true;
+    class TrainKerasPanel extends InteractiveEditorPanel {
+        constructor(layoutManager, params) {
+            const config = {
+                name: 'TrainKeras',
+                Control: TrainKerasControl,
+                Widget: TrainKerasWidget,
+            };
+            super(config, params);
 
-        //call parent's constructor
-        PanelBaseWithHeader.apply(this, [options, layoutManager]);
+            this.logger.debug('ctor finished');
+        }
 
-        this._client = params.client;
-        this._embedded = params.embedded;
+        /* OVERRIDE FROM WIDGET-WITH-HEADER */
+        /* METHOD CALLED WHEN THE WIDGET'S READ-ONLY PROPERTY CHANGES */
+        onReadOnlyChanged(isReadOnly) {
+            //apply parent's onReadOnlyChanged
+            PanelBaseWithHeader.prototype.onReadOnlyChanged.call(this, isReadOnly);
 
-        //initialize UI
-        this._initialize();
+        }
 
-        this.logger.debug('ctor finished');
+        onResize(width, height) {
+            this.logger.debug('onResize --> width: ' + width + ', height: ' + height);
+            this.widget.onWidgetContainerResize(width, height);
+        }
+
+        /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
+        destroy() {
+            this.control.destroy();
+            this.widget.destroy();
+
+            PanelBaseWithHeader.prototype.destroy.call(this);
+            WebGMEGlobal.KeyboardManager.setListener(undefined);
+            WebGMEGlobal.Toolbar.refresh();
+        }
+
+        onActivate() {
+            this.widget.onActivate();
+            this.control.onActivate();
+            WebGMEGlobal.KeyboardManager.setListener(this.widget);
+            WebGMEGlobal.Toolbar.refresh();
+        }
+
+        onDeactivate() {
+            this.widget.onDeactivate();
+            this.control.onDeactivate();
+            WebGMEGlobal.KeyboardManager.setListener(undefined);
+            WebGMEGlobal.Toolbar.refresh();
+        }
     }
-
-    //inherit from PanelBaseWithHeader
-    _.extend(TrainKerasPanel.prototype, PanelBaseWithHeader.prototype);
-    _.extend(TrainKerasPanel.prototype, IActivePanel.prototype);
-
-    TrainKerasPanel.prototype._initialize = function () {
-        var self = this;
-
-        //set Widget title
-        this.setTitle('');
-
-        this.widget = new TrainKerasWidget(this.logger, this.$el);
-
-        this.widget.setTitle = function (title) {
-            self.setTitle(title);
-        };
-
-        this.control = new TrainKerasControl({
-            logger: this.logger,
-            client: this._client,
-            embedded: this._embedded,
-            widget: this.widget
-        });
-
-        this.onActivate();
-    };
-
-    /* OVERRIDE FROM WIDGET-WITH-HEADER */
-    /* METHOD CALLED WHEN THE WIDGET'S READ-ONLY PROPERTY CHANGES */
-    TrainKerasPanel.prototype.onReadOnlyChanged = function (isReadOnly) {
-        //apply parent's onReadOnlyChanged
-        PanelBaseWithHeader.prototype.onReadOnlyChanged.call(this, isReadOnly);
-
-    };
-
-    TrainKerasPanel.prototype.onResize = function (width, height) {
-        this.logger.debug('onResize --> width: ' + width + ', height: ' + height);
-        this.widget.onWidgetContainerResize(width, height);
-    };
-
-    /* * * * * * * * Visualizer life cycle callbacks * * * * * * * */
-    TrainKerasPanel.prototype.destroy = function () {
-        this.control.destroy();
-        this.widget.destroy();
-
-        PanelBaseWithHeader.prototype.destroy.call(this);
-        WebGMEGlobal.KeyboardManager.setListener(undefined);
-        WebGMEGlobal.Toolbar.refresh();
-    };
-
-    TrainKerasPanel.prototype.onActivate = function () {
-        this.widget.onActivate();
-        this.control.onActivate();
-        WebGMEGlobal.KeyboardManager.setListener(this.widget);
-        WebGMEGlobal.Toolbar.refresh();
-    };
-
-    TrainKerasPanel.prototype.onDeactivate = function () {
-        this.widget.onDeactivate();
-        this.control.onDeactivate();
-        WebGMEGlobal.KeyboardManager.setListener(undefined);
-        WebGMEGlobal.Toolbar.refresh();
-    };
 
     return TrainKerasPanel;
 });
