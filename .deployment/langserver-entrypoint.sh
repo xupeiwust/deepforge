c0fc77fc3b9f3afa6cbcb3269ea6f683951f8399
Umesh Timalsina <umesh.timalsina@ioe.edu.np>
2020-10-02 12:17:36 -0500

    WIP-Fix jsonrpc-ws-proxy in langserver-entrypoint.sh

--- .deployment/langserver-entrypoint.sh
+++ .deployment/langserver-entrypoint.sh
@@ -4,5 +4,5 @@ source activate base
 pip uninstall tensorflow -y
 conda install tensorflow==1.14 -y
 
-node $(npm root -g)/jsonrpc-ws-proxy/dist/server.js --port $PORT --languageServers ~/language-servers.yml
+node jsonrpc-ws-proxy --port $PORT --languageServers ~/language-servers.yml
 
