880e1b2910806963fec2ce5d5f745368d25801a5
Brian Broll <brian.broll@gmail.com>
2020-11-02 11:43:08 -0600

    Add tabs, run action, initial operation

--- src/visualizers/panels/EagerOperation/EagerOperationControl.js
+++ src/visualizers/panels/EagerOperation/EagerOperationControl.js
@@ -2,13 +2,50 @@
 
 define([
     'panels/InteractiveExplorer/InteractiveExplorerControl',
+    'underscore',
+    'text!deepforge/NewOperationCode.ejs',
 ], function (
     InteractiveExplorerControl,
+    _,
+    NewOperationCodeTxt,
 ) {
 
     'use strict';
 
+    const GetOperationCode = _.template(NewOperationCodeTxt);
     class EagerOperationControl extends InteractiveExplorerControl {
+
+        constructor() {
+            super(...arguments);
+            const operation = this.getInitialOperation();
+            this._widget.setOperation(operation);
+        }
+
+        getInitialOperation() {
+            const basename = 'NewOperation';
+            let name = basename;
+            let i = '2';
+            const metanodes = Object.values(this.client.getAllMetaNodes());
+            while (metanodes.find(node => node.getAttribute('name') === name)) {
+                name = name + i++;
+            }
+
+            const code = GetOperationCode({name});
+
+            return {
+                name: name,
+                inputs: [],
+                outputs: [],
+                references: [],
+                code,
+                env: ''
+            };
+        }
+
+        async onComputeInitialized(session) {
+            await super.onComputeInitialized(session);
+            this._widget.registerActions();
+        }
     }
 
     return EagerOperationControl;
--- src/visualizers/widgets/EagerOperation/EagerOperationWidget.js
+++ src/visualizers/widgets/EagerOperation/EagerOperationWidget.js
@@ -1,9 +1,20 @@
-/*globals define */
+/*globals define, $, WebGMEGlobal*/
 
 define([
+    'deepforge/globals',
     'widgets/InteractiveEditor/InteractiveEditorWidget',
+    'widgets/OperationCodeEditor/OperationCodeEditorWidget',
+    'widgets/TabbedTextEditor/TabbedTextEditorWidget',
+    'widgets/TextEditor/TextEditorWidget',
+    'widgets/OperationInterfaceEditor/OperationInterfaceEditorWidget',
+    'css!./styles/EagerOperationWidget.css',
 ], function (
+    DeepForge,
     InteractiveEditor,
+    OperationCodeEditor,
+    TabbedTextEditorWidget,
+    TextEditorWidget,
+    OperationInterfaceEditorWidget,
 ) {
     'use strict';
 
@@ -12,8 +23,121 @@ define([
         constructor(logger, container) {
             container.addClass(WIDGET_CLASS);
             super(container);
+            this.width = 0;
+            this.height = 0;
+
+            const $leftPane = $('<div>', {class: 'pane'});
+            container.append($leftPane);
+            this.codeEditor = new OperationCodeEditor(logger, $leftPane);
+
+            const $rightPane = $('<div>', {class: 'pane'});
+            container.append($rightPane);
+            this.secondaryEditor = this.initializeSecondaryEditor(logger, $rightPane);
         }
-        // TODO: embed another widget
+
+        initializeSecondaryEditor(logger, $el) {
+            const config = {
+                canCreateTabs: false,
+                message: {
+                    new: '',
+                    empty: '',
+                    rename: '',
+                },
+            };
+            const widget = new TabbedTextEditorWidget(logger, $el, config);
+            this.tabs = [];
+
+            function newTab(name, $el, editor) {
+                return {
+                    id: name,
+                    name: name,
+                    supportedActions: {
+                        delete: false,
+                        rename: false,
+                    },
+                    editor,
+                    $el,
+                };
+            }
+            $el = $('<div>');
+            let editor = new OperationInterfaceEditorWidget(logger, $el);
+            this.tabs.push(newTab('Operation Interface', $el, editor));
+
+            $el = $('<div>');
+            editor = new TextEditorWidget(
+                logger,
+                $el,
+                {language: 'yaml', displayMiniMap: false}
+            );
+            this.tabs.push(newTab('Environment', $el, editor));
+
+            $el = $('<div>');
+            editor = new TextEditorWidget(
+                logger,
+                $el,
+                {language: 'plaintext'}
+            );
+            this.tabs.push(newTab('Console', $el, editor));
+
+            widget.onTabSelected = id => {
+                const tab = this.tabs.find(tab => tab.id === id);
+                this.onTabSelected(widget, tab);
+            };
+            this.tabs.forEach(tab => widget.addTab(tab));
+
+            return widget;
+        }
+
+        setOperation(operation) {
+            this.codeEditor.addNode({
+                name: operation.name,
+                text: operation.code,
+            });
+            const [interfaceTab, envTab] = this.tabs;
+            // TODO: update the interface editor
+
+            envTab.editor.addNode({name: operation.name, text: operation.env});
+        }
+
+        registerActions() {
+            // TODO: use the operation name
+            DeepForge.registerAction('Run operation', 'play_arrow', 10, () => this.runOperation());
+        }
+
+        runOperation() {
+            console.log('running operation!');  // TODO
+        }
+        // TODO: add interface editor
+        // TODO:   - add save button to outputs
+        // TODO:   - add input operations (and outputs?)
+        // TODO: add conda environment editor
+        // TODO: add console output tab
+        // TODO: add graphical output (controller?)
+        // TODO: add "save" button for the operation definition
+
+        onTabSelected(widget, tab) {
+            widget.$tabContent.empty();
+            widget.$tabContent.append(tab.$el);
+            const isDisplayed = this.width && this.height;
+            if (isDisplayed) {
+                this.onWidgetContainerResize(this.width, this.height);
+            }
+        }
+
+        onActivate() {
+            this.codeEditor.onActivate();
+            this.secondaryEditor.onActivate();
+        }
+
+        onWidgetContainerResize(width, height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        destroy() {
+            super.destroy();
+            DeepForge.unregisterAction('Run operation', 'play_arrow', 10, () => this.runOperation());
+        }
     }
 
     return EagerOperationWidget;
