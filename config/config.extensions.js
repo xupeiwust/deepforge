c8ee5473fabc855ddccbfbc4629a556f8ceb2e31
Brian Broll <brian.broll@gmail.com>
2021-01-07 08:26:31 -0600

    Ensure config "extensions" do not mutate the cached config. Fix #1985 (#1986)

--- config/config.extensions.js
+++ config/config.extensions.js
@@ -1,5 +1,7 @@
+const merge = require('lodash.merge');
 
 module.exports = config => {
+    config = merge({}, config);
     config.extensions = {};
     config.extensions.InteractiveComputeHost = process.env.DEEPFORGE_INTERACTIVE_COMPUTE_HOST;
     return config;
